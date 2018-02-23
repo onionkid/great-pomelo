5c45fd764658b78daa3cba72976fb56a048dd74e
kevin.l.arnado <kevin.l.arnado@CAZI-53607RR-LX.dir.svc.accenture.com>
2018-02-23 14:16:14 +0800

    great pomelo

---
+++ libfoo/foo.c
@@ -0,0 +1,19 @@
+#include "foo.h"
+#include <stdio.h>
+
+int dosomethingfoo(int x)
+{
+	int retVal=0;
+	if(x)
+	{
+		retVal = 1;
+		printf("it is foo true");
+	}
+	else
+	{
+		printf("it is not foo true");
+	}
+
+	return retVal;
+}
+
---
+++ main/main.c
@@ -0,0 +1,8 @@
+#include "foo.h"
+#include <stdio.h>
+
+int main()
+{
+	//this is the main function
+	printf("%d",dosomethingfoo(2));
+}
