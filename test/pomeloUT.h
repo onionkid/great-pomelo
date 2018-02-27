1cdd0cd95c74e65f92c7e1eea14aa5a610057a4d
kevin.l.arnado <kevin.l.arnado@CAZI-53607RR-LX.dir.svc.accenture.com>
2018-02-27 15:36:32 +0800

    h

--- test/pomeloUT.h
+++ test/pomeloUT.h
@@ -1,15 +1,15 @@
 #ifndef CUNIT_POMELO_H
 #define CUNIT_POMELO_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+//#ifdef __cplusplus
+//extern "C" {
+//#endif
 
 void AddTests(void);
 void print_example_results(void);
 
-#ifdef __cplusplus
-}
-#endif
+//#ifdef __cplusplus
+//}
+//#endif
 
 #endif
