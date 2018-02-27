19aa3f16f2bf94ef168215e24f9581628a3bc145
kevin.l.arnado <kevin.l.arnado@CAZI-53607RR-LX.dir.svc.accenture.com>
2018-02-27 14:06:30 +0800

    cunit test

--- test/mainTest.c
+++ test/mainTest.c
@@ -1,13 +1,59 @@
 #include "foo.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[])
+#include "Automated.h"
+#include "pomeloUT.h"
+
+int main(int argc, char* argv[])
 {
-	int retVal = 0;
+  CU_BOOL Run = CU_FALSE ;
 
+  setvbuf(stdout, NULL, _IONBF, 0);
 
-	//this is the main function
-	//printf("\n%d\n",dosomethingfoo(2));
+  if (argc > 1) {
+    if (!strcmp("-i", argv[1])) {
+      Run = CU_TRUE ;
+      CU_set_error_action(CUEA_IGNORE);
+    }
+    else if (!strcmp("-f", argv[1])) {
+      Run = CU_TRUE ;
+      CU_set_error_action(CUEA_FAIL);
+    }
+    else if (!strcmp("-A", argv[1])) {
+      Run = CU_TRUE ;
+      CU_set_error_action(CUEA_ABORT);
+    }
+    else if (!strcmp("-e", argv[1])) {
+      print_example_results();
+    }
+    else {
+      printf("\nUsage:  AutomatedTest [option]\n\n"
+               "        Options: -i  Run, ignoring framework errors [default].\n"
+               "                 -f  Run, failing on framework error.\n"
+               "                 -A  Run, aborting on framework error.\n"
+               "                 -e  Print expected test results and exit.\n"
+               "                 -h  Print this message.\n\n");
+    }
+  }
+  else {
+    Run = CU_TRUE;
+    CU_set_error_action(CUEA_IGNORE);
+  }
 
-	return retVal;
+  if (CU_TRUE == Run) {
+    if (CU_initialize_registry()) {
+      printf("\nInitialization of Test Registry failed.");
+    }
+    else {
+      AddTests();
+      CU_set_output_filename("GreatPomeloUT");
+      CU_list_tests_to_file();
+      CU_automated_run_tests();
+      CU_cleanup_registry();
+    }
+  }
+
+  return 0;
 }
---
+++ test/pomeloUT.h
@@ -0,0 +1,15 @@
+#ifndef CUNIT_POMELO_H
+#define CUNIT_POMELO_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void AddTests(void);
+void print_example_results(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
